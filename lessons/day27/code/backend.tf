9f37ced4d93cb43880449b84d1dff20468337123
venkatasai987 <venkatsai2036@gmail.com>
2026-02-19 22:53:46 +0530

    b

--- lessons/day27/code/backend.tf
+++ lessons/day27/code/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "staging-my-terraform-bucket-saydhw" # Replace with your S3 bucket name
-    key          = "terraform/state/main/terraform.tfstate"
+    bucket       = "terraform-state-1771520713" # Replace with your S3 bucket name
+    key          = "env/terraform.tfstate"
     region       = "us-east-1" # Replace with your region
     use_lockfile = true        # S3 Native Locking (No DynamoDB needed)
     encrypt      = true
