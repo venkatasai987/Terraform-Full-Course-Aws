0875cec71917a9b32679ff7fdf1818fd7ce4dd4e
venkatasai987 <venkatsai2036@gmail.com>
2026-02-19 23:00:07 +0530

    b11

--- lessons/day27/code/alb.tf
+++ lessons/day27/code/alb.tf
@@ -9,7 +9,7 @@ resource "aws_lb" "app_lb" {
   idle_timeout               = 60
 
   tags = {
-    Name = "app-load-balancer-${var.environment}"
+    Name = "app1-load-balancer-${var.environment}"
   }
 }
 
